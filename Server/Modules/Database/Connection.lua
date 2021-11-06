9346ec688c784f70a15628eb9debed43a9fee3d9
Adam Greenwood <agreenwood1305@gmail.com>
2021-11-06 19:45:13 +0000

    Added initial playerdata table query

--- Server/Modules/Database/Connection.lua
+++ Server/Modules/Database/Connection.lua
@@ -10,5 +10,20 @@ if not dbConnection or not dbConnection:ping() then
     end)
 
 else
+    local query = string.format( [[
+        CREATE TABLE IF NOT EXISTS `playerdata` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `rpname` varchar(50) NOT NULL DEFAULT '',
+        `money` int(11) NOT NULL DEFAULT 0,
+        `steamid` varchar(50) NOT NULL DEFAULT '0',
+        `position` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+        `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `steamid` (`steamid`),
+        UNIQUE KEY `rpname` (`rpname`)
+    )
+    ]] )
+    dbConnection:execute(query)
+    
     Package.Log("[ Aperture ] Database initialised")
 end
